ad5692f1425065a01d7a2e87b5bf9f45a89f6273
Jonas Bergmann <bergmann.jonas31@gmail.com>
2023-05-04 17:47:24 +0200

    Add more (debug) tests for block alloc

--- user/malloc_test_cxx.cc
+++ user/malloc_test_cxx.cc
@@ -67,6 +67,54 @@ void test_balloc(void) {
     auto foo = block_alloc_typed<char>(0);
     block_free(foo.untyped);
   }
+  {
+    auto foo = BALLOC(int, 1000);
+    if (foo.begin) {
+        int *bar = static_cast<int*>(foo.begin);
+      int sum = bar[0] + bar[1];
+      (void) sum;
+      memset(foo.begin, 0, 20);
+    }
+    auto foo2 = BALLOC(int, 1000);
+    if (foo2.begin) {
+        int *bar = static_cast<int*>(foo2.begin);
+      int sum = bar[0] + bar[1];
+      (void) sum;
+      memset(foo2.begin, 0, 20);
+    }
+    auto foo3 = BALLOC(int, 1000);
+    if (foo3.begin) {
+        int *bar = static_cast<int*>(foo3.begin);
+      int sum = bar[0] + bar[1];
+      (void) sum;
+      memset(foo3.begin, 0, 20);
+    }
+    block_free(foo);
+    block_free(foo2);
+    block_free(foo3);
+  }
+  {
+    printf("block alloc 1 Byte with alignment 1024... ");
+    auto foo = block_alloc(1, 1024);
+    printf("got ptr %p\n", foo.begin);
+    if (foo.begin) {
+      int *bar = static_cast<int*>(foo.begin);
+      int sum = bar[0] + bar[1];
+      (void) sum;
+      memset(foo.begin, 0, 20);
+    }
+  }
+  {
+    printf("block alloc 1 Byte with alignment 1000... ");
+    auto foo = block_alloc(1, (1 << 13) - 1);
+    printf("got ptr %p\n", foo.begin);
+    if (foo.begin) {
+      int *bar = static_cast<int*>(foo.begin);
+      int sum = bar[0] + bar[1];
+      (void) sum;
+      memset(foo.begin, 0, 20);
+    }
+  }
 }
 
 void main(int argc, char **argv) {
