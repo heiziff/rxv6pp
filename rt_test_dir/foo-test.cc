a2114c7dc137216a0428c83bd4ecb0551322dce3
Peter Maucher <peter.maucher@kit.edu>
2023-05-05 19:04:20 +0200

    added test directory with custom init script and simple test case

---
+++ rt_test_dir/foo-test.cc
@@ -0,0 +1,7 @@
+#include "user/user.h"
+
+int main() {
+	// this is a pointless test
+	printf("footest executes\n");
+	return 0;
+}
---
+++ rt_test_dir/init.cc
@@ -0,0 +1,51 @@
+// init: The initial user-level program
+
+#include "kernel/file.h"
+#include "user/user.h"
+#include "kernel/fcntl.h"
+
+const char *argv[] = { "sh", 0 };
+
+bool substring(const char *pattern, const char *string) {
+  int pattern_index = 0;
+  int index = 0;
+  while (string[index]) {
+    if (!pattern[pattern_index]) return true;
+    if (string[index] == pattern[pattern_index]) {
+      ++pattern_index;
+    }
+    else
+      pattern_index = 0;
+    ++index;
+  }
+  return !pattern[pattern_index];
+}
+
+int main(void) {
+  if(open("console", O_RDWR) < 0){
+    mknod("console", CONSOLE, 0);
+    open("console", O_RDWR);
+  }
+  dup(0);  // stdout
+  dup(0);  // stderr
+
+  // look up testcases
+  int fd = open("/", O_RDONLY);
+
+  for (dirent entry; read(fd, &entry, sizeof(dirent)) == sizeof(dirent); ) {
+    if(!entry.name[0]) continue;
+    if (substring("test", entry.name)) {
+      printf("starting test [%s]\n", entry.name);
+      auto pid  = fork();
+      if(pid < 0) exit(1);
+      else if (pid == 0)
+        exec(entry.name, const_cast<char**>(argv));
+      auto ret = wait(0);
+      if (ret != pid) exit(1); // we're not really init
+    }
+  }
+
+  // leads to a crash, but who really cares?
+  printf("finished tests, crashing now\n");
+  exit(0);
+}
