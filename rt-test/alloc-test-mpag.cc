d6e649d6bd814eaaf0f98718bd655a5308d2fb85
Peter Maucher <peter.maucher@kit.edu>
2023-05-10 19:29:20 +0200

    split up testcase+cleanup

--- rt-test/alloc-test-mpag.cc
+++ rt-test/alloc-test-mpag.cc
@@ -7,35 +7,11 @@
 #include "rt-test/assert.h"
 #include "user/bmalloc.h"
 
-void test_align(void *ptr, int alignment) {
-	auto val = reinterpret_cast<uint64>(ptr);
-	if (alignment & 1)
-		return;
-	if (alignment & 2) {
-		assert((val & 1) == 0);
-		return;
-	}
-	if (alignment & 4) {
-		assert((val & 3) == 0);
-		return;
-	}
-	if (alignment & 8) {
-		assert((val & 7) == 0);
-		return;
-	}
-	assert((val & 15));
-}
-
 void main() {
 	for (int i = 4096; i < 1 << 16; ++i) {
 		void * ptr = malloc(i);
 		assert(ptr);
 		assert(!(reinterpret_cast<uint64>(ptr) & ((1 << 12) - 1)));
 		free(ptr);
-
-		auto block = block_alloc(i, 4096);
-		assert(block.begin);
-		assert(!(reinterpret_cast<uint64>(block.begin) & ((1 << 12) - 1)));
-		block_free(block);
 	}
 }
