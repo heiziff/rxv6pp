32e8683856497e752ed475da4d873eb52c37ece1
Peter Maucher <peter.maucher@kit.edu>
2023-05-10 19:35:09 +0200

    a lot of mmap testcases

---
+++ rt-test/mmap-test-inva.cpp
@@ -0,0 +1,19 @@
+/*!
+ * \file
+ * \brief test that invalid m(un)map calls fail
+ */
+
+#include <rt-test/assert.h>
+#include <user/mmap.h>
+
+void main() {
+	int i = 0;
+
+	int *ptr = &i;
+	if(reinterpret_cast<uint64>(ptr) & (PAGE_SIZE - 1)) ++ptr;
+
+	assert(mmap(ptr, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0) == MAP_FAILED);
+	assert(mmap(0, 0, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0) == MAP_FAILED);
+
+	assert(munmap(ptr, 4096));
+}
