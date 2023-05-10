9c0aa150662e072aebbe23f0f4e857340dd2aac1
Peter Maucher <peter.maucher@kit.edu>
2023-05-10 19:31:16 +0200

    implement more requirements from the manpage

--- user/mmap-mock.cc
+++ user/mmap-mock.cc
@@ -26,7 +26,10 @@ void *mmap(void *addr, uint64 length, int prot, int flags, int fd, uint64 offset
 	if (length & (PAGE_SIZE - 1)) return MAP_FAILED;
 	if (offset) return MAP_FAILED;
 	if (prot != (PROT_READ | PROT_WRITE)) return MAP_FAILED;
-	if (flags != (MAP_PRIVATE | MAP_ANONYMOUS)) return MAP_FAILED;
+	if ((flags != (MAP_PRIVATE | MAP_ANONYMOUS)) &&
+		(flags != (MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE)))
+		return MAP_FAILED;
+	if (!length) return MAP_FAILED;
 
 	void *ptr = malloc(length + PAGE_SIZE);
 	if (!ptr) return MAP_FAILED;
@@ -43,6 +46,7 @@ void *mmap(void *addr, uint64 length, int prot, int flags, int fd, uint64 offset
 
 int munmap(void *addr, uint64 length) {
 	if (!addr) return 0;
+	if (reinterpret_cast<uint64>(addr) & (PAGE_SIZE - 1)) return 1;
 
 	for (unsigned i = 0; i < memsize; ++i) {
 		if (memlist[i].page_begin == addr) {
