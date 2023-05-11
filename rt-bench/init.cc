812416b5de42d6efdc057450b1883c9027fb5f23
Peter Maucher <peter.maucher@kit.edu>
2023-05-11 15:56:50 +0200

    better framework

--- rt-bench/init.cc
+++ rt-bench/init.cc
@@ -32,20 +32,48 @@ int main(void) {
   int fd = open("/", O_RDONLY);
 
   for (dirent entry; read(fd, &entry, sizeof(dirent)) == sizeof(dirent);) {
-    if (!entry.name[0]) continue;
-    if (substring("bench", entry.name)) {
-      printf("starting benchmark [%s]\n", entry.name);
+    char readbuffer[DIRSIZ + 1] = {};
+    memcpy(readbuffer, entry.name, DIRSIZ);
+    if (!readbuffer[0]) continue;
+    if (substring("bench", readbuffer)) {
+      printf(
+        "\033[;34m"
+        ">>>> starting benchmark [%s]\n"
+        "\033[0m",
+        readbuffer);
       auto pid = fork();
       if (pid < 0)
         exit(1);
-      else if (pid == 0)
-        exec(entry.name, const_cast<char **>(argv));
-      auto ret = wait(0);
+      else if (pid == 0) {
+        exec(readbuffer, const_cast<char **>(argv));
+        printf(
+          "\033[1;31m"
+          ">>>> exit failed\n"
+          "\033[0m");
+        exit(1);
+      }
+      int retcode = 0;
+      auto ret    = wait(&retcode);
       if (ret != pid) exit(1); // we're not really init
+      if (retcode)
+        printf(
+          "\033[1;31m"
+          ">>>> testcase [%s] failed with %d\n"
+          "\033[0m",
+          readbuffer, retcode);
+      else
+        printf(
+          "\033[1;32m"
+          ">>>> testcase [%s] succeeded\n"
+          "\033[0m",
+          readbuffer, retcode);
     }
   }
 
   // leads to a crash, but who really cares?
-  printf("finished tests, crashing now\n");
+  printf(
+    "\033[;34m"
+    ">>>> finished benchmark, crashing now\n"
+    "\033[0m");
   exit(0);
 }
