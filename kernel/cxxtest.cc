34a93a05f17be55dde898777d00f726d33319949
Peter Maucher <peter.maucher@kit.edu>
2023-05-11 11:34:46 +0200

    formatting

--- kernel/cxxtest.cc
+++ kernel/cxxtest.cc
@@ -2,15 +2,14 @@
 
 template<int What>
 void a_template() {
-	printf(const_cast<char*>("cpp printed: %d\n"), What);
+  printf(const_cast<char *>("cpp printed: %d\n"), What);
 }
-extern "C"
-uint64 sys_cxx() {
-	int argnr;
-	argint(0, &argnr);
-	if(argnr & 1)
-		a_template<1>();
-	else
-		a_template<0>();
-	return 0;
+extern "C" uint64 sys_cxx() {
+  int argnr;
+  argint(0, &argnr);
+  if (argnr & 1)
+    a_template<1>();
+  else
+    a_template<0>();
+  return 0;
 }
