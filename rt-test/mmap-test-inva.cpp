cf1a0fa02eb63738e8d8969ab875d82c63c8b4ca
Peter Maucher <peter.maucher@kit.edu>
2023-05-12 09:59:37 +0200

    fix test case

--- rt-test/mmap-test-inva.cpp
+++ rt-test/mmap-test-inva.cpp
@@ -9,11 +9,9 @@
 void main() {
   int i = 0;
 
-  int *ptr = &i;
-  if (reinterpret_cast<uint64>(ptr) & (PAGE_SIZE - 1)) ++ptr;
-
-  assert(mmap(ptr, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0) == MAP_FAILED);
   assert(mmap(0, 0, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0) == MAP_FAILED);
 
+  int *ptr = &i;
+  if (reinterpret_cast<uint64>(ptr) & (PAGE_SIZE - 1)) ++ptr;
   assert(munmap(ptr, 4096));
 }
