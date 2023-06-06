461aed2937908e9decc24406dbd8cfc5d85f062a
Jonas Bergmann <bergmann.jonas31@gmail.com>
2023-06-06 13:56:08 +0200

    Fix another peter test

--- rt-test/mmap-test-inva.cpp
+++ rt-test/mmap-test-inva.cpp
@@ -6,14 +6,16 @@
 #include <rt-test/assert.h>
 #include <user/user.h>
 
-void main() {
-	int i = 0;
+void main()
+{
+    int i = 0;
 
-	int *ptr = &i;
-	if(reinterpret_cast<uint64>(ptr) & (PAGE_SIZE - 1)) ++ptr;
+    int *ptr = &i;
+    if (!(reinterpret_cast<uint64>(ptr) & (PAGE_SIZE - 1)))
+        ++ptr;
 
-	assert(mmap(ptr, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS) == (void*) MAP_FAILED);
-	assert(mmap(0, 0, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS) == (void*) MAP_FAILED);
+    assert(mmap(ptr, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED) == (void *)MAP_FAILED);
+    assert(mmap(0, 0, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS) == (void *)MAP_FAILED);
 
-	assert(munmap(ptr, 4096));
+    assert(munmap(ptr, 4096));
 }
