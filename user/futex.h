5fff42947a9f5f6a6f1e115cfcd1d81f09608f98
Peter Maucher <peter.maucher@kit.edu>
2023-06-16 18:11:00 +0200

    C++ shenanigans

--- user/futex.h
+++ user/futex.h
@@ -9,11 +9,31 @@
 extern "C" {
 #endif
 
-//! the mutex type
-typedef _Atomic unsigned long long osdev_mutex_t; // TODO: change
-
 //! not technically needed, but heavily suggested
 typedef unsigned long long osdev_mutex_underlying_t;
+
+#ifndef __cplusplus
+
+#include <stdbool.h>
+
+typedef _Atomic osdev_mutex_underlying_t osdev_mutex_t;
+
+#else
+
+//! \brief C++ struct for wrapping
+//! \attention disabled some C++ features to  lead to somewhat sane behavior
+//! \attention never instantiate this class directly, only ever via a pointer or to instantiate
+//! \attention only here for alignment/size purposes, ignore that
+struct osdev_mutex_t {
+    osdev_mutex_underlying_t inner;
+    osdev_mutex_t() = default;
+    osdev_mutex_t(const osdev_mutex_t&) = delete;
+    osdev_mutex_t(osdev_mutex_t&&) = delete;
+    osdev_mutex_t& operator=(const osdev_mutex_t&) = delete;
+    osdev_mutex_t& operator=(osdev_mutex_t&&) = delete;
+};
+
+#endif
 
 //! initialize mutex
 void osdev_mutex_init(osdev_mutex_t *mutex);
@@ -24,5 +44,10 @@ void osdev_mutex_unlock(osdev_mutex_t *mutex);
 
 //! try lock mutex
 bool osdev_mutex_trylock(osdev_mutex_t *mutex);
+
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif
