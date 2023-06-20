d328a1931f008aec43cc883cac89cd55ccaa11da
Peter Maucher <peter.maucher@kit.edu>
2023-06-20 10:37:33 +0200

    updated to new standards

--- user/futex.c
+++ user/futex.c
@@ -2,17 +2,17 @@
 #include <stdatomic.h>
 
 void osdev_mutex_init(osdev_mutex_t *mutex) {
-    *mutex = 0;
+    mutex->inner = 0;
 }
 
 void osdev_mutex_lock(osdev_mutex_t *mutex) {
-    osdev_mutex_t other = 0;
-    while(!atomic_compare_exchange_weak(mutex, &other, 1))
-        other = 0;
+    osdev_mutex_t other = {0};
+    while(!atomic_compare_exchange_weak(&mutex->inner, &other.inner, 1))
+        other.inner = 0;
 }
 //! unlock mutex
 void osdev_mutex_unlock(osdev_mutex_t *mutex) {
-    atomic_store(mutex, 0);
+    atomic_store(&mutex->inner, 0);
 }
 
 //! try lock mutex
