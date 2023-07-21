f60601664a9d0211777502c0dc64df81dfa5ca32
Felix Zimmer <felix.zimmer@student.kit.edu>
2023-07-21 09:33:45 +0000

    Continue IP implementation

--- kernel/ip.c
+++ kernel/ip.c
@@ -1,6 +1,7 @@
 #include "ip.h"
 #include "ethernet.h"
 #include "defs.h"
+#include "arp.h"
 
 uint8 own_ip[4] = {10, 0, 2, 42};
 
@@ -41,10 +42,29 @@ void ip_send_packet(uint8 *dst_ip, uint8 *data, uint8 protocol, uint32 length) {
     // Lastly, compute checksum
     packet->checksum = ip_checksum(packet);
 
+    // now, copy data
+    uint8 *datagram_data = ((uint8*) packet) + sizeof(ip_datagram);
+    memcpy(datagram_data, data, length);
 
+    //Check, if mac address is known:
+    uint8 *mac_addr = arp_lookup(dst_ip);
+    if (!mac_addr) {
+        // if mac is unknown, get ready to ARP:
+        // TODO: maybe try more than once?
+        arp_send_packet(bcast_haddr, dst_ip);
+        // TODO: sleep until response
 
+        // After wakeup: lookup and send:
+        mac_addr = arp_lookup(dst_ip);
 
+        if (!mac_addr) {
+            printk(" IP_send: couldn't find out target hwaddr!\n");
+            return;
+        }
+    }
 
+    // MAC is known, send ethernet packet
+    ethernet_send_packet(mac_addr, packet, ETH_TYPE_IP, length + sizeof(ip_datagram));
 
-
+    kfree(packet);
 }
